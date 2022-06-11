5e0697da219fe22fb618bb5ddecefdbe6b26fd60
w0lv3rin3n3 <robert_valentin.ene@stud.etti.upb.ro>
2022-06-12 00:46:15 +0300

    created database, added data to db, created graphs, added buttons

--- licenta_ene_robert/Front-End/app/app.js
+++ licenta_ene_robert/Front-End/app/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const SerialPort = require("serialport");
-// var fs = require("fs");
 const mysql = require("mysql");
 const app = express();
 const cors = require("cors");
 const { json } = require("express");
+const moment = require("moment");
 app.use(cors());
 
 const parsers = SerialPort.parsers;
@@ -13,23 +13,28 @@ const parser = new parsers.Readline({
   delimiter: "\r\n",
 });
 
-var port = new SerialPort("COM10", {
-  baudRate: 115200,
-  dataBits: 8,
-  parity: "none",
-  stopBits: 1,
-  flowControl: false,
-});
+// Create port
+// var port = new SerialPort("COM10", {
+//   baudRate: 115200,
+//   dataBits: 8,
+//   parity: "none",
+//   stopBits: 1,
+//   flowControl: false,
+// });
+// port.pipe(parser);
 
+
+// Create data buffer
 var dataFromSensor = {};
 // var dataFromSensor = new Map();
 
-port.pipe(parser);
+
 /*sensorData = [] array of objects
 sensorData.push(data) inside parser function
 example: sensorData[0].temperature (results in number)
 */
 
+// Parse JSON incoming data
 parser.on("data", function (data) {
   // let sensor_params = data.split("@");
   // const sensor_id = sensor_params[0];
@@ -48,25 +53,162 @@ parser.on("data", function (data) {
   console.log("Received data from port: " + data);
   app.emit("data", data); //this should send the object to index.html
   dataFromSensor = obj;
+  addDataToDb(dataFromSensor)
 });
 
+// Create connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Mucidegaina.99',
+  database: 'esp_sensors_db'
+  // port: 3306
+}) 
+
+// Connect to MySql
+db.connect(err => {
+  if(err) {
+    throw err
+  }
+  console.log('MySQL Connected');
+})
+
+// Create Database
+app.get('/createdb', (req, res) => {
+  let sql = 'CREATE DATABASE esp_sensors_db'
+  db.query(sql, err => {
+    if(err) {
+      throw err;
+    }
+    res.send('Database Created');
+  })
+})
+
+// Create Table
+app.get('/create-sensors-table', (req, res) => {
+  let sql = 'CREATE TABLE sensors(id int AUTO_INCREMENT, sensor_name VARCHAR(255), arg1 VARCHAR(255), arg2 VARCHAR(255), PRIMARY KEY(id))'
+  db.query(sql, err => {
+    if(err) {
+      throw err;
+    }
+    res.send('Sensors table Created');
+  })
+})
+
+// Insert sensor
+// app.get('/insert-sensor', (req, res) => {
+// var today = moment().toDate()
+// today = moment(today).format('YYYY-MM-DD HH:mm:ss')
+//   let post = {sensor_name: `temp`, arg1: '25', arg2: ``, date: `${today}`};
+//   let sql = 'INSERT INTO sensors SET ?'
+//   let query = db.query(sql, post, err => {
+//     if(err) {
+//       throw err
+//     }
+//     res.send('Sensor added');
+//   })
+// })
+
+function addDataToDb(dataFromSensor) {
+  var today = moment().toDate()
+  today = moment(today).format('YYYY-MM-DD HH:mm:ss')
+  let post = {
+    sensor_name: `temp`,
+    arg1: dataFromSensor.temperature,
+    arg2: dataFromSensor.humidity,
+    date: today
+  };
+  let sql = 'INSERT INTO sensors SET ?'
+  let query = db.query(sql, post, err => {
+    if(err) {
+      throw err
+    }
+  })
+}
+
+// Select sensor
+app.get('/get-sensor', (req, res) => {
+  let sql = 'SELECT * from sensors'
+  let query = db.query(sql, (err, results) => {
+    if(err) {
+      throw err
+    }
+    console.log(results);
+    res.send('Sensor details fetched');
+  })
+})
+
+// Update sensor
+app.get('/update-sensor/:id', (req, res) => {
+  let newName = 'Updated name';
+  let sql = `UPDATE sensors SET sensor_name = '${newName}' WHERE id = ${req.params.id}`
+  let query = db.query(sql, err => {
+    if(err) {
+      throw err
+    }
+    res.send('Sensor updated');
+  })
+})
+
+// Delete sensor
+app.get('/delete-sensor/:id', (req, res) => {
+  let newName = 'Updated name';
+  let sql = `DELETE FROM sensors WHERE id = ${req.params.id}`
+  let query = db.query(sql, err => {
+    if(err) {
+      throw err
+    }
+    res.send('Sensor deleted');
+  })
+})
+
+// Root
 app.get("/", (req, res) => {
   res.send("Server running on express");
 });
 
-app.get("/temperature-sensor", (req, res) => {
-  res.send(dataFromSensor);
+// Get temperature data
+app.get("/dht11-sensor", (req, res) => {
+  let sql = `SELECT * from sensors WHERE sensor_name = 'dht11'`
+  let query = db.query(sql, (err, results) => {
+    if(err) {
+      throw err
+    }
+    for(let element of results) {
+      element.date = moment(element.date).format('YYYY-MM-DD HH:mm:ss')
+    }
+    res.send(results);
+  })
 });
 
-app.get("/2nd-sensor", (req, res) => {
-  res.send(dataFromSensor);
+// Get 2nd-sensor data
+app.get("/gas-sensor", (req, res) => {
+  let sql = `SELECT * from sensors WHERE sensor_name = 'gas_sensor'`
+  let query = db.query(sql, (err, results) => {
+    if(err) {
+      throw err
+    }
+    for(let element of results) {
+      element.date = moment(element.date).format('YYYY-MM-DD HH:mm:ss')
+    }
+    res.send(results);
+  })
 });
 
+// Set ledState
 app.get("/slider-input/send/:value", (req, res) => {
   const ledState = req.params.value;
   port.write(`{"ledState":${ledState}}`);
   // port.write(ledState);
   res.send(`Led state changed to ${ledState}`);
+});
+
+// Set commandIR
+app.get("/commandIR/send/:value", (req, res) => {
+  const commandIR = req.params.value;
+  port.write(`{"commandIR":${commandIR}}`);
+  // port.write(ledState);
+  res.send(`Command ${commandIR} is sent to TV`);
 });
 /*
 tables: sensor1 [1], sensor2 [2]
---
+++ licenta_ene_robert/Front-End/app/src/feedTemp.js
@@ -0,0 +1,66 @@
+const feedDisplayTemp = document.querySelector("#feedTemp");
+fetch("http://localhost:3000/dht11-sensor")
+  .then((response) => response.json())
+  .then((data) => {
+    const ctx = document.getElementById("myChartTemp").getContext("2d");
+    const dateTimeArr = [];
+    for(let element of data) {
+      // moment(element.date).format('HH:mm')
+      dateTimeArr.push(element.date);
+    }
+    const valuesArr = [];
+    for(let element of data) {
+      if(element.sensor_name == 'temp')
+        valuesArr.push(element.arg1);
+    }
+    const myChartTemp = new Chart(ctx, {
+      type: "line",
+      data: {
+        //array of strings (datetimes)
+        labels: dateTimeArr,
+        datasets: [
+          {
+            label: "Temperature",
+            //array of datetime values
+            data: valuesArr, 
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+    feedDisplayTemp.insertAdjacentHTML(
+      "afterbegin",
+      "Temperature: " + data[data.length-1].arg1 + "<br>Humidity: " + data[data.length-1].arg2 + "<br>Date: " + data[data.length-1].date
+    );
+  })
+  .catch((err) => console.log(err));
+
+
+
+  setTimeout(function(){
+    window.location.reload(1);
+ }, 20000);
